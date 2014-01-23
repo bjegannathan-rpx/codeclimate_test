80745c193b8e0eac71e77e2209722ca187234d1a
Bragadeesh J <bjegannathan@rpxcorp.com>
2014-01-24 00:34:43 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeclimateTest::Application.config.secret_token = '278281b209fa5a7f8ab85263bcd4fe09824320264d00c5299349572e107b0da6c35a6bfbf266a9eff73ee78a0c3ebfe7a4a571ea8185837d4844e0f6c107f17f'
