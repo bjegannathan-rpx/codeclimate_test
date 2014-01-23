f6a8575428896c3df727bce4c6bfe63bf693aace
Bragadeesh J <bjegannathan@rpxcorp.com>
2014-01-24 00:53:01 +0530

    added a test

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_injections
+  	assert_equal [{"id"=>1, 0=>1}], Project.injection1
+  	assert_equal 1, Project.injection2.size
+  end
 end
