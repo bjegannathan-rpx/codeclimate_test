2a8cfc36d6440ccc8f4eb9798dc8330ba838f0f1
Bragadeesh J <bjegannathan@rpxcorp.com>
2014-01-24 00:44:05 +0530

    A test for code climate

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,14 @@
 class Project < ActiveRecord::Base
   attr_accessible :title
+
+  def self.injection1(arg=1)
+    k = connection.execute(<<-SQL
+      SELECT id FROM projects where id in (#{arg})
+    SQL
+    ).to_a
+  end
+
+  def self.injection2(arg=1)
+    Project.find_by_sql("SELECT ID FROM projects where id in (#{arg})")
+  end
 end
