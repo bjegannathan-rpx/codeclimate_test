35f84e866f489d5cd06be6a1d3e3a972312e8e40
Bragadeesh J <bjegannathan@rpxcorp.com>
2014-02-04 16:49:35 -0800

    Brakeman missing the ActiveRecord

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,4 +11,13 @@ class Project < ActiveRecord::Base
   def self.injection2(arg=1)
     Project.find_by_sql("SELECT ID FROM projects where id in (#{arg})")
   end
+
+  
+  def self.injection3(arg=1)
+    k = ActiveRecord::Base.connection.execute(<<-SQL
+      SELECT id FROM projects where id in (#{arg})
+    SQL
+    ).to_a
+  end
+
 end
